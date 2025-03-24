400a7f2e21ac86961c4c85594103843c69f541e2
Jan Tom Förster <jan_tom@f-rster.de>
2025-03-24 20:17:40 +0100

    Added a read method in WorldManager.java to put them in ram instead of making them usable for players and multiple options for the methods that already exist.

--- src/main/java/me/gaminglounge/racemc/WorldManager.java
+++ src/main/java/me/gaminglounge/racemc/WorldManager.java
@@ -18,23 +18,59 @@ public class WorldManager {
     private final AdvancedSlimePaperAPI asp = AdvancedSlimePaperAPI.instance();
     SlimeLoader loader = RaceMC.INSTANCE.getLoader();
     SlimePropertyMap blankPropertys = new SlimePropertyMap();
+    SlimeWorld world;
+
     
 
-
-    public void loadWorld(String worldname) {
-        blankPropertys.setValue(SlimeProperties.WORLD_TYPE, "flat");
-        SlimeWorld world;
+    /**
+     * Load a world from the loader folder.
+     * @param worldname The name of the world to load.
+     * @param worldPropertys Optional propertymap for the world to load, else gona return blank.
+     */
+    public void loadWorld(String worldname, SlimePropertyMap worldPropertys) {
         try {
-            world = asp.readWorld(loader, worldname, false, blankPropertys);
+            world = asp.readWorld(loader, worldname, false, worldPropertys);
             asp.loadWorld(world, false);
             Bukkit.getLogger().info("Loaded "+worldname.toString()+" successfully.");
         } catch (CorruptedWorldException | NewerFormatException | IOException e) {
             e.printStackTrace();
         }
         catch (UnknownWorldException e) {
-            world = asp.createEmptyWorld(worldname, false, blankPropertys, loader);
+            world = asp.createEmptyWorld(worldname, false, worldPropertys, loader);
             asp.loadWorld(world, false);
             Bukkit.getLogger().warning("The world "+worldname.toString()+"does not exist, creating a blank world.");
         }
     }
+    public void loadWorld(String worldname){
+        blankPropertys.setValue(SlimeProperties.WORLD_TYPE, "flat");
+        loadWorld(worldname, blankPropertys);
+    }
+
+
+/**
+ * Read a world from the loader folder.
+ * @param worldname The name of the world to read.
+ * @param worldProperty Optional propertymap for the world to read, else gona return blank.
+ */
+    public void readworld(String worldname, SlimePropertyMap worldProperty){
+        try {
+            world = asp.readWorld(loader, worldname, false, worldProperty);
+            Bukkit.getLogger().info("Loaded "+worldname.toString()+" successfully.");
+        } catch (CorruptedWorldException | NewerFormatException | IOException e) {
+            e.printStackTrace();
+        }
+        catch (UnknownWorldException e) {
+            world = asp.createEmptyWorld(worldname, false, worldProperty, loader);
+            Bukkit.getLogger().warning("The world "+worldname.toString()+"does not exist, creating a blank world.");
+        }
+    }
+    public void readworld(String worldname){
+        blankPropertys.setValue(SlimeProperties.WORLD_TYPE, "flat");
+        readworld(worldname, blankPropertys);
+    }
+
+    public void copyChunks(String fromWorld, String toWorld, Integer fromX, Integer fromZ, Integer toX, Integer toZ){
+
+    }
+
 }
