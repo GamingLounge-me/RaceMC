f3534288c333087a08bb184a5041f3b16c61491e
JanTomFoerster <jan_tom@f-rster.de>
2025-05-27 17:21:20 +0200

    Just pushing what ever has been done

--- src/main/java/me/gaminglounge/racemc/WorldManager.java
+++ src/main/java/me/gaminglounge/racemc/WorldManager.java
@@ -5,6 +5,8 @@ import com.infernalsuite.asp.api.exceptions.CorruptedWorldException;
 import com.infernalsuite.asp.api.exceptions.NewerFormatException;
 import com.infernalsuite.asp.api.exceptions.UnknownWorldException;
 import com.infernalsuite.asp.api.loaders.SlimeLoader;
+import com.infernalsuite.asp.api.world.SlimeChunk;
+import com.infernalsuite.asp.api.world.SlimeChunkSection;
 import com.infernalsuite.asp.api.world.SlimeWorld;
 import com.infernalsuite.asp.api.world.properties.SlimeProperties;
 import com.infernalsuite.asp.api.world.properties.SlimePropertyMap;
@@ -26,8 +28,9 @@ public class WorldManager {
      * Load a world from the loader folder.
      * @param worldname The name of the world to load.
      * @param worldPropertys Optional propertymap for the world to load, else gona return blank.
+     * @return The world that was read.
      */
-    public void loadWorld(String worldname, SlimePropertyMap worldPropertys) {
+    public SlimeWorld loadWorld(String worldname, SlimePropertyMap worldPropertys) {
         try {
             world = asp.readWorld(loader, worldname, false, worldPropertys);
             asp.loadWorld(world, false);
@@ -40,10 +43,11 @@ public class WorldManager {
             asp.loadWorld(world, false);
             Bukkit.getLogger().warning("The world "+worldname.toString()+"does not exist, creating a blank world.");
         }
+        return world;
     }
-    public void loadWorld(String worldname){
+    public SlimeWorld loadWorld(String worldname){
         blankPropertys.setValue(SlimeProperties.WORLD_TYPE, "flat");
-        loadWorld(worldname, blankPropertys);
+        return loadWorld(worldname, blankPropertys);
     }
 
 
@@ -51,8 +55,9 @@ public class WorldManager {
  * Read a world from the loader folder.
  * @param worldname The name of the world to read.
  * @param worldProperty Optional propertymap for the world to read, else gona return blank.
+ * @return The world that was read.
  */
-    public void readworld(String worldname, SlimePropertyMap worldProperty){
+    public SlimeWorld readworld(String worldname, SlimePropertyMap worldProperty){
         try {
             world = asp.readWorld(loader, worldname, false, worldProperty);
             Bukkit.getLogger().info("Loaded "+worldname.toString()+" successfully.");
@@ -63,14 +68,22 @@ public class WorldManager {
             world = asp.createEmptyWorld(worldname, false, worldProperty, loader);
             Bukkit.getLogger().warning("The world "+worldname.toString()+"does not exist, creating a blank world.");
         }
+        return world;
     }
-    public void readworld(String worldname){
+    public SlimeWorld readworld(String worldname){
         blankPropertys.setValue(SlimeProperties.WORLD_TYPE, "flat");
-        readworld(worldname, blankPropertys);
+        return readworld(worldname, blankPropertys);
     }
 
-    public void copyChunks(String fromWorld, String toWorld, Integer fromX, Integer fromZ, Integer toX, Integer toZ){
+
+
+    public void copyChunks(String fromWorld, String toWorld){
+
+        SlimeWorld fWorld = readworld(fromWorld);
+        SlimeWorld tWorld = readworld(toWorld);
+        SlimeChunk test = fWorld.getChunk(0, 0);
 
     }
+
 
 }
